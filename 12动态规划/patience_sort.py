8326c37487eb3f610216903bc93494ecb72d8b20
Alwin.Zhang <alwin114@hotmail.com>
2022-03-31 17:41:06 +0800

    chore: update comment of method

--- 12动态规划/patience_sort.py
+++ 12动态规划/patience_sort.py
@@ -1,21 +1,23 @@
 def lis(nums):
     n = len(nums)
+    # 最坏状态是纸牌正序排列，有多少个纸牌拆多少个堆  top数组用于维护所有堆的堆顶最小点数的纸牌
     top = [0] * n
     piles = 0
     for i in range(n):
         poker = nums[i]
+        # left每次都是从0开始，right就是堆的个数，每次的任务就是把当前的poker通过二分查找放进该放的堆中
         left, right = 0, piles
         while left < right:
             mid = (left + right) // 2
-            if top[mid] > poker:
+            if top[mid] > poker: # 当mid位置的纸牌点数≥poker时，当前的poker放入mid左边的堆
                 right = mid
-            elif top[mid] < poker:
+            elif top[mid] < poker: # 放入mid右边的堆
                 left = mid + 1
             else:
                 right = mid
-        if left == piles:
+        if left == piles: # 当左边界和堆的个数相同时，需要新开一个堆
             piles += 1
-        top[left] = poker
+        top[left] = poker # 更新poker放入的堆顶元素
     return piles
 
 
